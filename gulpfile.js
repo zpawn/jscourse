ea0bd7eeef4f011eaf6235a4e086e06a0c13af1e
zpawn <ilia.makarov@me.com>
2017-02-20 12:52:22 +0200

    gulp: add sourceMap

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
 const debug = require('gulp-debug');
 
 /**
@@ -10,7 +11,10 @@ const debug = require('gulp-debug');
  */
 gulp.task('sass', function() {
     return gulp.src('30-gulp/frontend/**/app.scss')
+//        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+//        .pipe(sourcemaps.write())   // add sourceMap in same file
+//        .pipe(sourcemaps.write('.'))   // add sourceMap in single file
         .pipe(concat('app.css'))
         .pipe(gulp.dest('30-gulp/public/css'))
 });
